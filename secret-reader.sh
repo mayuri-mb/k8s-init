b6fb0b501dc9e24edf96bb7018db5c8d56b9ac8b
mayuri <mayu.reddy10@gmail.com>
2026-05-08 16:12:53 +1000

    k8s

--- secret-reader.sh
+++ secret-reader.sh
@@ -4,15 +4,15 @@
 #2. if exists, read it and export it, then call mysql entrypoint script
 #3. If does not exist, throw the error
 
-if [ -f /secrets/mysql-secret-reader.txt]; then
-    PASSWORD = $(cat /secrets/mysql-secret-reader.txt)
+if [ -f /secrets/mysql-secret-reader.txt ]; then
+    PASSWORD=$(cat /secrets/mysql-secret-reader.txt)
     echo "password accessed successfully"
 else
     echo "can't find the secret file"
     exit 1
 fi 
 
-export MYSQL_ROOT_PASSWORD = $PASSWORD
+export MYSQL_ROOT_PASSWORD=$PASSWORD
 rm /secrets/mysql-secret-reader.txt
 
 exec /usr/local/bin/docker-entrypoint.sh mysqld
